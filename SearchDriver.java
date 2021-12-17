7d8b9507df4c9b094d6f188b6e7a4fb6b5929ec4
Nada Hameed <nada@Nadas-MacBook-Air.local>
2021-12-17 15:11:34 -0500

    just impresando la hora

--- SearchDriver.java
+++ SearchDriver.java
@@ -1,3 +1,8 @@
 public class SearchDriver{
-  
+  public static void main(String[] args){
+    long a = System.currentTimeMillis();
+    System.out.println(a);
+    long b = System.currentTimeMillis();
+    System.out.println(b);
+  }
 }
